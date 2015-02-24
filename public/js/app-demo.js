a619e935ee9bfc828aaa7b5e438e5ad507519869
Teresa Nededog <tnededog@my.hpu.edu>
2015-02-23 14:21:19 -1000

    indentation corrections

--- public/js/app-demo.js
+++ public/js/app-demo.js
@@ -1,42 +1,42 @@
 $(function() {
 
-    $(" p").click(function() {
-      $( this ).text( $( this ).html() );
-    });
+  $(" p").click(function() {
+    $( this ).text( $( this ).html() );
+  });
 
-    $(".appendText").click(function(){
-        $("p").append("<b> Appended text</b>.");
-    });
-  
-    $(".appendItem").click(function(){
-        $("ol").append("<li><b>Appended item</b></li>");
-    });
+  $(".appendText").click(function(){
+    $("p").append("<b> Appended text</b>.");
+  });
 
-    $(".appendToText").click(function(){
-        $("<b> AppendTo text</b>.").appendTo("p");
-    });
-  
-    $(".appendToItem").click(function(){
-        $("<li><b>AppendTo item</b></li>").appendTo("ol");
-    });
-        
-    //PREPENDS
-    
-    $(".prependText").click(function(){
-      $("p").prepend("<b> prepended text</b>.");
-    });
-  
-    $(".prependItem").click(function(){
-      $("ol").prepend("<li><b>prepended item</b></li>");
-    });
+  $(".appendItem").click(function(){
+    $("ol").append("<li><b>Appended item</b></li>");
+  });
 
-    $(".prependToText").click(function(){
-      $("<b> prependTo text</b>.").prependTo("p");
-    });
-  
-    $(".prependToItem").click(function(){
-      $("<li><b>prependTo item</b></li>").prependTo("ol");
-    });
+  $(".appendToText").click(function(){
+    $("<b> AppendTo text</b>.").appendTo("p");
+  });
+
+  $(".appendToItem").click(function(){
+    $("<li><b>AppendTo item</b></li>").appendTo("ol");
+  });
+      
+  //PREPENDS
+
+  $(".prependText").click(function(){
+    $("p").prepend("<b> prepended text</b>.");
+  });
+
+  $(".prependItem").click(function(){
+    $("ol").prepend("<li><b>prepended item</b></li>");
+  });
+
+  $(".prependToText").click(function(){
+    $("<b> prependTo text</b>.").prependTo("p");
+  });
+
+  $(".prependToItem").click(function(){
+    $("<li><b>prependTo item</b></li>").prependTo("ol");
+  });
   
 });
 
